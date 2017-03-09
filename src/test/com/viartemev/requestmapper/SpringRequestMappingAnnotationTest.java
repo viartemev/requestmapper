1e2db0e6054f2d1137feb76b93e022420e96c784
Vyacheslav Artemyev <viartemev@gmail.com>
2017-03-09 17:47:25 +0300

    Change strucure of project & add test path

---
+++ src/test/com/viartemev/requestmapper/SpringRequestMappingAnnotationTest.java
@@ -0,0 +1,13 @@
+package com.viartemev.requestmapper;
+
+import org.junit.Test;
+
+//TODO add tests
+public class SpringRequestMappingAnnotationTest {
+
+    @Test
+    public void values() throws Exception {
+
+    }
+
+}

9e0e8be5e2e62f0958595f4508af51356bbd09d8
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-11-01 15:49:53 +0200

    add junit5

---
+++ src/test/kotlin/RequestMappingModelTest.kt
@@ -0,0 +1,10 @@
+package com.viartemev.requestmapper
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RequestMappingModelTest {
+    @org.junit.jupiter.api.Test
+    fun matches() {
+    }
+
+}

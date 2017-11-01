e52099f907633d0805cde5b61e14566e40d7af36
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-11-01 16:36:46 +0200

    add test for match method

--- src/test/kotlin/RequestMappingModelTest.kt
+++ src/test/kotlin/RequestMappingModelTest.kt
@@ -1,10 +1,45 @@
-package com.viartemev.requestmapper
+import com.intellij.openapi.command.impl.DummyProject
+import com.viartemev.requestmapper.RequestMappingModel
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
+class RequestMappingModelTest {
 
-internal class RequestMappingModelTest {
-    @org.junit.jupiter.api.Test
-    fun matches() {
+    val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+
+    @Test
+    fun `one slash`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/"))
+    }
+
+    @Test
+    fun `word without slash`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "product"))
+    }
+
+    @Test
+    fun `one first part of path`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/api"))
+    }
+
+    @Test
+    fun `one middle part of path`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/product"))
+    }
+
+    @Test
+    fun `two first parts of path`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/api/v1"))
+    }
+
+    @Test
+    fun `two first parts of path with slash on the end`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/api/v1/"))
+    }
+
+    @Test
+    fun `two middle parts of path with slash on the end`() {
+        assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/product/123"))
     }
 
 }

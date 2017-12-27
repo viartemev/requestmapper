19181ef4e9eca4f9219896eba7993980a06b89f8
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-12-27 17:02:32 +0200

    fix hierarchy

--- src/main/kotlin/com/viartemev/requestmapper/annotations/spring/PostMapping.kt
+++ src/main/kotlin/com/viartemev/requestmapper/annotations/spring/PostMapping.kt
@@ -2,7 +2,7 @@ package com.viartemev.requestmapper.annotations.spring
 
 import com.intellij.psi.PsiAnnotation
 
-class PostMapping(psiAnnotation: PsiAnnotation) : SpringMappingAnnotation(psiAnnotation) {
+class PostMapping(psiAnnotation: PsiAnnotation) : RequestMapping(psiAnnotation) {
 
     override fun extractMethod() = METHOD
 

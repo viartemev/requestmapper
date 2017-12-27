ace903b1871471d9225b3a65f1f467d64116fc0a
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-12-27 14:34:02 +0200

    add kluent

--- src/test/kotlin/com/viartemev/requestmapper/utils/CommonUtilsSpek.kt
+++ src/test/kotlin/com/viartemev/requestmapper/utils/CommonUtilsSpek.kt
@@ -1,101 +1,101 @@
 package com.viartemev.requestmapper.utils
 
-import com.intellij.testFramework.UsefulTestCase.assertEmpty
+import org.amshove.kluent.shouldBeEmpty
+import org.amshove.kluent.shouldBeEqualTo
+import org.amshove.kluent.shouldBeFalse
+import org.amshove.kluent.shouldBeTrue
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
 
 object CommonUtilsSpek : Spek({
 
     describe("a string") {
         on("unquote empty string") {
             it("should return empty string") {
-                assertEmpty("".unquote())
+                "".unquote().shouldBeEmpty()
             }
         }
         on("unquote string without quotes") {
             val str = "string without quote"
             it("should return original string") {
-                assertEquals(str.unquote(), str)
+                str.unquote() shouldBeEqualTo str
             }
         }
         on("unquote string on quoted string") {
             it("should return string without quotes") {
-                assertEquals("\"string\"".unquote(), "string")
+                "\"string\"".unquote() shouldBeEqualTo "string"
             }
         }
         on("unquote string with one quote at the start") {
             val str = "\"string"
             it("should return original string") {
-                assertEquals(str.unquote(), str)
+                str.unquote() shouldBeEqualTo str
             }
         }
         on("unquote string with one quote at the end") {
             val str = "string\""
             it("should return original string") {
-                assertEquals(str.unquote(), str)
+                str.unquote() shouldBeEqualTo str
             }
         }
         on("unquote string with one quote at the middle") {
             val str = "string \" another string"
             it("should return original string") {
-                assertEquals(str.unquote(), str)
+                str.unquote() shouldBeEqualTo str
             }
         }
         on("unquote empty quoted string") {
             it("should return empty string") {
-                assertEmpty("\"\"".unquote())
+                "\"\"".unquote().shouldBeEmpty()
             }
         }
         on("unquote string with one quote") {
             val str = "\""
             it("should return original string") {
-                assertEquals(str.unquote(), str)
+                str.unquote() shouldBeEqualTo str
             }
         }
 
         on("inCurlyBrackets on empty string") {
             it("should return false") {
-                assertFalse("".inCurlyBrackets())
+                "".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on string without brackets") {
             it("should return false") {
-                assertFalse("string without brackets".inCurlyBrackets())
+                "string without brackets".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on string in curly brackets") {
             it("should return true") {
-                assertTrue("{hello}".inCurlyBrackets())
+                "{hello}".inCurlyBrackets().shouldBeTrue()
             }
         }
         on("inCurlyBrackets on string in curly brackets in reverse order") {
             it("should return false") {
-                assertFalse("}hello{".inCurlyBrackets())
+                "}hello{".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on string with only first curly bracket") {
             it("should return false") {
-                assertFalse("{hello".inCurlyBrackets())
+                "{hello".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on string with only last curly bracket") {
             it("should return false") {
-                assertFalse("hello}".inCurlyBrackets())
+                "hello}".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on string in curly brackets in the middle of string") {
             it("should return false") {
-                assertFalse("hello this {is} test spring".inCurlyBrackets())
+                "hello this {is} test spring".inCurlyBrackets().shouldBeFalse()
             }
         }
         on("inCurlyBrackets on empty string in curly brackets") {
             it("should return true") {
-                assertTrue("{}".inCurlyBrackets())
+                "{}".inCurlyBrackets().shouldBeTrue()
             }
         }
     }

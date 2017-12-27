81d378262d80e2f6de6126b43fe8f534bb8e50b8
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2017-12-27 13:44:08 +0200

    move RequestMapping test to spek

---
+++ src/test/kotlin/com/viartemev/requestmapper/RequestMappingModelTest.kt
@@ -0,0 +1,50 @@
+package com.viartemev.requestmapper
+
+import com.intellij.openapi.command.impl.DummyProject
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertTrue
+
+object RequestMappingModelSpeck : Spek({
+
+    describe("RequestMappingModel") {
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/"))
+            }
+        }
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "product"))
+            }
+        }
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/api"))
+            }
+        }
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/product"))
+            }
+        }
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/api/v1"))
+            }
+        }
+        on("TODO") {
+            val requestMappingModel = RequestMappingModel(DummyProject.getInstance())
+            it("should return true") {
+                assertTrue(requestMappingModel.matches("POST /api/v1/product/{product-id}/items/{item-id}", "/product/123"))
+            }
+        }
+    }
+})

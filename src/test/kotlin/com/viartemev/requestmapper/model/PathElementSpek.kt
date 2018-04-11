6d3a78452f30262b20e514e2aa11d276cce1cad6
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2018-04-11 14:31:44 +0300

    add tests

--- src/test/kotlin/com/viartemev/requestmapper/model/PathElementSpek.kt
+++ src/test/kotlin/com/viartemev/requestmapper/model/PathElementSpek.kt
@@ -8,10 +8,11 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import java.math.BigInteger
 
 object PathElementSpek : Spek({
     describe("PathElement") {
-        on("hasPathVariable with path in curly brackets") {
+        on("hasPathVariable with a path element in curly brackets") {
             it("should return true") {
                 PathElement("{id}").isPathVariable.shouldBeTrue()
             }
@@ -22,106 +23,116 @@ object PathElementSpek : Spek({
             }
         }
 
-        on("addPathVariableType on path element without curly brackets") {
-            it("should return original element") {
+        on("addPathVariableType on a path element without curly brackets") {
+            it("should return the original element") {
                 val originalPath = "id"
                 PathElement(originalPath).addPathVariableType("String").value shouldEqual originalPath
             }
         }
-        on("addPathVariableType on path element with curly brackets") {
-            it("should return original element with type") {
+        on("addPathVariableType on a path element with curly brackets") {
+            it("should return the original element with a type") {
                 PathElement("{id}").addPathVariableType("String").value shouldEqual "{String:id}"
             }
         }
-        on("addPathVariableType on path element with curly brackets with blank type") {
-            it("should return original element with type String") {
+        on("addPathVariableType on a path element with curly brackets with a blank type") {
+            it("should return the original element with a type String") {
                 PathElement("{id}").addPathVariableType("").value shouldEqual "{String:id}"
             }
         }
 
-        on("equals with empty element") {
+        on("equals with an empty element") {
+            it("should return false") {
+                PathElement("api") shouldNotEqual PathElement("")
+            }
+        }
+        on("equals with the same string") {
             it("should return true") {
                 PathElement("api") shouldEqual PathElement("api")
             }
         }
-        on("equals with string") {
-            it("should return true") {
-                PathElement("api") shouldEqual PathElement("api")
-            }
-        }
-        on("equals on element with String type path variable and digital") {
+        on("equals on an element with a String type path variable and digits") {
             it("should return true") {
                 PathElement("{String:id}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with String type path variable and string") {
+        on("equals on an element with a String type path variable and a string") {
             it("should return true") {
                 PathElement("{String:id}") shouldEqual PathElement("string")
             }
         }
-        on("equals on element with Long type path variable and digital") {
+        on("equals on an element with Long type path variable and digits") {
             it("should return true") {
                 PathElement("{Long:id}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with Long type path variable and string") {
+        on("equals on an element with Long type path variable and a string") {
             it("should return false") {
                 PathElement("{Long:id}") shouldNotEqual PathElement("string")
             }
         }
-        on("equals on element with String type path variable and digital with inverted order") {
+        on("equals on an element with a String type path variable and digits (inverted order)") {
             it("should return true") {
                 PathElement("123") shouldEqual PathElement("{String:id}")
             }
         }
-        on("equals on element with String type path variable and string with inverted order") {
+        on("equals on an element with a String type path variable and a string (inverted order)") {
             it("should return true") {
                 PathElement("string") shouldEqual PathElement("{String:id}")
             }
         }
-        on("equals on element with Long type path variable and digital with inverted order") {
+        on("equals on an element with a Long type path variable and digits (inverted order)") {
             it("should return true") {
                 PathElement("123") shouldEqual PathElement("{Long:id}")
             }
         }
-        on("equals on element with Long type path variable and string with inverted order") {
+        on("equals on an element with a Long type path variable and a string (inverted order)") {
             it("should return false") {
                 PathElement("string") shouldNotEqual PathElement("{Long:id}")
             }
         }
-        on("equals on element with regex (more than one any single character) path variable and string") {
+        on("equals on an element with a regex (more than one any single character) path variable and a string") {
             it("should return true") {
                 PathElement("{String:id:.+}") shouldEqual PathElement("abc")
             }
         }
-        on("equals on element with regex (only digits) path variable and string") {
+        on("equals on an element with a regex (only digits) path variable and a string") {
             it("should return false") {
                 PathElement("{String:id:[\\\\d]+}") shouldNotEqual PathElement("abc")
             }
         }
-        on("equals on element with regex (only digits) path variable and digits") {
+        on("equals on an element with a regex (only digits) path variable and digits") {
             it("should return true") {
                 PathElement("{String:id:[\\\\d]+}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with Integer type path variable and digital") {
+        on("equals on an element with a Integer type path variable and digits") {
             it("should return true") {
                 PathElement("{Integer:id}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with int type path variable and digital") {
+        on("equals on an element with int type path variable and digits") {
             it("should return true") {
                 PathElement("{int:id}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with long type path variable and digital") {
+        on("equals on an element with a long type path variable and digits") {
             it("should return true") {
                 PathElement("{long:id}") shouldEqual PathElement("123")
             }
         }
-        on("equals on element with BigInteger type path variable and digital") {
+        on("equals on an element with a BigInteger type path variable and digits") {
             it("should return true") {
                 PathElement("{BigInteger:id}") shouldEqual PathElement("123")
+            }
+        }
+        on("equals on an element with null") {
+            it("should return false") {
+                PathElement("{BigInteger:id}") shouldNotEqual null
+            }
+        }
+        on("equals on an element with an element different class") {
+            it("should return false") {
+                PathElement("{BigInteger:id}") shouldNotEqual BigInteger("1")
             }
         }
     }
--- src/test/kotlin/com/viartemev/requestmapper/utils/AnnotationUtilsSpek.kt
+++ src/test/kotlin/com/viartemev/requestmapper/utils/AnnotationUtilsSpek.kt
@@ -2,11 +2,14 @@ package com.viartemev.requestmapper.utils
 
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import org.amshove.kluent.shouldBeFalse
 import org.amshove.kluent.shouldBeTrue
+import org.amshove.kluent.shouldEqual
+import org.amshove.kluent.shouldThrow
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -14,13 +17,13 @@ import org.jetbrains.spek.api.dsl.on
 
 object AnnotationUtilsSpek : Spek({
     describe("AnnotationUtils") {
-        on("isMethodAnnotation on annotation without class and method") {
+        on("isMethodAnnotation on an annotation without a class and a method") {
             it("should return false") {
                 val annotation = mock<PsiAnnotation> {}
                 annotation.isMethodAnnotation().shouldBeFalse()
             }
         }
-        on("isMethodAnnotation on class annotation") {
+        on("isMethodAnnotation on a class annotation") {
             it("should return false") {
                 val clazz = mock<PsiClass> {}
                 val annotation = mock<PsiAnnotation> {
@@ -29,13 +32,41 @@ object AnnotationUtilsSpek : Spek({
                 annotation.isMethodAnnotation().shouldBeFalse()
             }
         }
-        on("isMethodAnnotation on method annotation") {
-            it("should return false") {
+        on("isMethodAnnotation on a method annotation") {
+            it("should return true") {
                 val method = mock<PsiMethod> {}
                 val annotation = mock<PsiAnnotation> {
                     on { parent } doReturn method
                 }
                 annotation.isMethodAnnotation().shouldBeTrue()
+            }
+        }
+        on("isMethodAnnotation on an unknown parent") {
+            it("should return false") {
+                val method = mock<PsiElement> {}
+                val annotation = mock<PsiAnnotation> {
+                    on { parent } doReturn method
+                }
+                annotation.isMethodAnnotation().shouldBeFalse()
+            }
+        }
+        on("fetchAnnotatedMethod on an unknown parent") {
+            it("should throw ClassCastException") {
+                val method = mock<PsiElement> {}
+                val annotation = mock<PsiAnnotation> {
+                    on { parent } doReturn method
+                }
+                val fetchAnnotatedMethod = { annotation.fetchAnnotatedMethod() }
+                fetchAnnotatedMethod shouldThrow ClassCastException::class
+            }
+        }
+        on("fetchAnnotatedMethod on a method annotation") {
+            it("should return the method") {
+                val method = mock<PsiMethod> {}
+                val annotation = mock<PsiAnnotation> {
+                    on { parent } doReturn method
+                }
+                annotation.fetchAnnotatedMethod() shouldEqual method
             }
         }
     }
